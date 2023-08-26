0b490ed097efc9bfc745f082ae01edf12f028117
sergiogallegos <gallegoshinojosa@gmail.com>
2023-08-26 12:04:04 -0600

    sg

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,16 +5,9 @@ const responseElement = document.getElementById('response');
 
 fetchButton.addEventListener('click', async () => {
     try {
-        // Change the text of the button while fetching data
-        fetchButton.textContent = 'Fetching...';
-
         const data = await fetchHello();
-
         // Update the content of the response element
         responseElement.textContent = data;
-
-        // Change the text of the button back to its original text
-        fetchButton.textContent = 'Fetch Data';
     } catch (error) {
         console.error('Error:', error);
 
